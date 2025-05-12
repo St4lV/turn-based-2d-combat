a1f8148494f89e1943521db5f25d31aa9f8a332a
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-12 07:15:50 +0200

    upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747025492|14864620650';
+const CACHE_VERSION = '1747026921|16293848727';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
