646ad67bd2b7d65910799c1c94171531d70c3579
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-12 11:45:43 +0200

    fixed en_us translations keys

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747029615|18987035744';
+const CACHE_VERSION = '1747043107|2851024925';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
