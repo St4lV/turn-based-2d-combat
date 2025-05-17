f44f758cabd26291f610a11919b7af18d8d7d59b
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-17 16:39:12 +0200

    progress saved when game is closed

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747491146|3145403821';
+const CACHE_VERSION = '1747492717|4715701404';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
