a8cc67cc7d762ffdbe3c818d440aa21fc4f49182
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-17 15:15:37 +0200

    screen mode toggle

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747293645|230003112';
+const CACHE_VERSION = '1747487714|16402072034';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
