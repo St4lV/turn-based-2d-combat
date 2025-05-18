0bf7e5b2d936cb5e3d9db1d10ed34448fdf04487
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-18 17:35:34 +0200

    predifned teams selection

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747505814|7372594345';
+const CACHE_VERSION = '1747582517|13269985704';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
