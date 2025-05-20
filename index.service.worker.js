d787e0d26f7d2e6a66a8536efb25224ffa4d8a5f
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-20 23:14:51 +0200

    punsihment & heresy -> smite

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747761585|8212799775';
+const CACHE_VERSION = '1747775658|12387675940';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
