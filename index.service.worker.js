c63469bdb82d2ed48d4e2d3792a8136ff92a7b22
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-20 19:21:20 +0200

    Weapons for models !
    Models can now handle weapon with weapon_r or weapon_l as name for playables as for enemies.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747721517|4516320673';
+const CACHE_VERSION = '1747761585|8212799775';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
