b3893d7083dca9de012df0d7978756c9d9a0fe02
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-20 08:14:38 +0200

    diverse tweaks
    add poisoned effect texture;
    zombie nerf
    priest buff
    fixed capacities cooldown handler
    completed missing translations keys

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747582517|13269985704';
+const CACHE_VERSION = '1747721517|4516320673';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
