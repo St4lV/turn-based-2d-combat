d7a77f7cf9255ba184e5e382794c7feb2aa29dc6
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-22 01:26:11 +0200

    fixed movement in level that was broken in export

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747858928|33920909809';
+const CACHE_VERSION = '1747869928|6209352910';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
