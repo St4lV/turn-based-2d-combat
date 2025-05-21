a85e341f709190aa3d3f54c3dd77d3a963b0383c
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-21 22:22:24 +0200

    add level map

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747775848|12577260278';
+const CACHE_VERSION = '1747858928|33920909809';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
