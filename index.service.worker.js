eec1d4908520fab2e1e3f0f6319838f2c7dad413
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-22 23:14:59 +0200

    implemented event handler

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747873158|9439488202';
+const CACHE_VERSION = '1747948466|24535441393';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
