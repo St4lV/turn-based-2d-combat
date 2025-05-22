c8c7a757bf394cb81009a25aad4da56e70ae0eed
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-22 02:19:39 +0200

    case spec display

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747869928|6209352910';
+const CACHE_VERSION = '1747873158|9439488202';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
