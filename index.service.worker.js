22dd0db8575f7c0092336d0c8874913a5159cabb
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-23 21:08:45 +0200

    fixed interface position in level

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748025190|21380785448';
+const CACHE_VERSION = '1748027300|23490498921';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
