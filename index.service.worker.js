5be3158e0ff1411f6e46e0cb72be6496909b440e
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-23 20:35:11 +0200

    fixed encounter crash + increased camera zoom in level

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748021639|17829965197';
+const CACHE_VERSION = '1748025190|21380785448';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
