25b10f9eb9f286e0fcbb6870a0fc5ef61c558693
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-23 19:34:43 +0200

    support humans as ennemies

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748006684|2874921119';
+const CACHE_VERSION = '1748021639|17829965197';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
