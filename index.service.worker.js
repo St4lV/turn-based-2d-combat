1e92355a9de25b0feba8e62910f0807a2426bea2
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-24 20:18:07 +0200

    add health field in add_team event action + disabled capacities instead of not showing

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748041009|37199275669';
+const CACHE_VERSION = '1748110590|3428471524';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
