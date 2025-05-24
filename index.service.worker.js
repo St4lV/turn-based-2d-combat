9fb8a47388a54d98c512b1b07fa1d17e92900bef
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-24 22:58:23 +0200

    add menu settings and traductions

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748110590|3428471524';
+const CACHE_VERSION = '1748120261|13099328172';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
