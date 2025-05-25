66ed0ab640c3d2e566ec39ee2c2d47d46fd16183
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-25 10:24:58 +0200

    add new effect type :  vampirism

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748120261|13099328172';
+const CACHE_VERSION = '1748161425|54262684317';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
