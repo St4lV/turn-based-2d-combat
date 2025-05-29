68031d2cc3953e7c38d4a6a272d4d2ec3ea73c2c
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-29 12:11:55 +0200

    add of effects relics
    fixed action ordering

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748473183|14881238057';
+const CACHE_VERSION = '1748513183|9451133808';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
