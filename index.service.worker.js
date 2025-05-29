366c375794da46e29dd6e21a6586b83eb7b9c690
st4l <152074070+St4lV@users.noreply.github.com>
2025-05-29 12:58:18 +0200

    rooted and nature wand trad keys + relic gold support with damage

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748513183|9451133808';
+const CACHE_VERSION = '1748516287|12554710157';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
