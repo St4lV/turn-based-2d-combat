20a06cc3349ba751bee8ad22dbcb80079bcbe7f0
st4l <152074070+St4lV@users.noreply.github.com>
2025-06-01 17:27:45 +0200

    started requirements to implement shops

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748516287|12554710157';
+const CACHE_VERSION = '1748791594|8802552873';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg-tour-par-tou-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
